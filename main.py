aae4c694b48bbe06dfc402aae5e446d8a511045d
Betty Chitty <42243427+DataBettyLBRB@users.noreply.github.com>
2021-08-03 13:55:06 -0400

    Adding the groupby function to calculate the COUNT and CASE

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 
 
 def main():
-
-    cols = ['VISN', 'DoDAAC', 'EXPECTED_DATE_OF_NEED']
+    cols = ['VISN', 'DoDAAC', 'EXPECTED_DATE_OF_NEED', 'EACH', 'CASE']
 
     results = reader.read_NCRT_files(cols)
 
@@ -21,24 +20,45 @@ def main():
     visn_col = 'VISN'
     dodaac_col = 'DoDAAC'
     jul_col = 'julian'
+    each_col = 'EACH'
+    case_col = 'CASE'
 
     results[date_col] = datefile.object_datetime(results[date_col])
     results[jul_col] = datefile.datetime_julian(results[date_col]).astype('str')
-    results['requisition'] = results[visn_col].astype('str')+\
-                             results[dodaac_col].astype('str')+\
+    results['requisition'] = results[visn_col].astype('str') + \
+                             results[dodaac_col].astype('str') + \
                              results[jul_col].astype('str')
 
-    cols = ['A', 'B', 'C']
-    a = results['requisition']
-    b = results[visn_col]
-    c = results[date_col]
+    final_results = results.groupby(['requisition', 'VISN', 'DoDAAC']).agg({each_col: sum,
+                                                                            case_col: sum}).reset_index()
 
-    rows = [a, b, c]
+    data_cols = ['Requisition', 'VISN', 'DoDAAC', 'Count of EACH', 'Count of CASE']
 
-    put_html('<h3>Edited Data Goes Here</h3>')
+    req = results['requisition']
+    visn = results[visn_col]
+    dodaac = results[dodaac_col]
+    each = results[each_col]
+    case = results[case_col]
+
+    req_f = final_results['requisition']
+    visn_f = final_results[visn_col]
+    dodaac_f = final_results[dodaac_col]
+    each_f = final_results[each_col]
+    case_f = final_results[case_col]
+
+    rows_1 = [req, visn, dodaac, each, case]
+    rows_2 = [req_f, visn_f, dodaac_f, each_f, case_f]
+
+    put_html('<h3>Original Data</h3>')
     put_table([
-        cols,
-        rows
+        data_cols,
+        rows_1
+    ])
+
+    put_html('<h3>Summarized Data</h3>')
+    put_table([
+        data_cols,
+        rows_2
     ])
 
 
